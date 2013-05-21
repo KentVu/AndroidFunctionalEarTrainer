0bd0bd34f8f60af52a7f4fedaed47a064d677fb0
Kien Vu <vutrankien.vn@gmail.com>
2013-05-21 11:33:40 +0700

    Test trả lời các câu hỏi thử, chuẩn bị thống kê bằng sqlite3

--- src/fetel/hcmus/earpractice/MainActivity.java
+++ src/fetel/hcmus/earpractice/MainActivity.java
@@ -1,17 +1,30 @@
 package fetel.hcmus.earpractice;
 
-import android.os.Bundle;
+import java.util.Random;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+	Random rand;
+	Integer mockquestion = 0;
+	TextView mockhint;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		rand = new Random();
+		mockhint = (TextView) findViewById(R.id.mockhint);
+		// mockhint.setText("1");
 	}
 
 	@Override
@@ -20,10 +33,46 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	public void onClickedButtonSetting(View v)
-	{
-		Intent intent1 = new Intent (MainActivity.this, SettingActivity.class);
+
+	public void onClickedButtonSetting(View v) {
+		Intent intent1 = new Intent(MainActivity.this, SettingActivity.class);
 		startActivity(intent1);
 	}
 
+	public void onBtnsClick(View v) {
+		// mockhint = (TextView) findViewById(R.id.mockhint);
+		TextView result = (TextView) findViewById(R.id.result);
+		Button b = (Button) v;
+
+		switch (v.getId()) {
+		case R.id.btn1:
+		case R.id.btn2:
+		case R.id.btn3:
+		case R.id.btn4:
+		case R.id.btn5:
+		case R.id.btn6:
+		case R.id.btn7:
+		case R.id.btn8:
+		case R.id.btn9:
+		case R.id.btn10:
+		case R.id.btn11:
+		case R.id.btn12:
+			if (Integer.parseInt(b.getText().toString()) == mockquestion) {
+				result.setText(getResources().getString(R.string.correct));
+				newMockQuestion();
+			} else {
+				result.setText(getResources().getString(R.string.notcorrect));
+			}
+			break;
+		case R.id.start:
+			newMockQuestion();
+			break;
+		}
+	}
+
+	private void newMockQuestion() {
+		// generate random answer:
+		mockquestion = rand.nextInt(11);
+		mockhint.setText(mockquestion.toString());
+	}
 }
