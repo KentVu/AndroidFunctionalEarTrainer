26c32e2af80fdf9e8ea9a5a5b27667375d226bcb
Kien Vu <vutrankien.vn@gmail.com>
2013-06-24 08:06:39 +0700

    use mediaPlayer to play simple midi file note

--- src/fetel/hcmus/earpractice/MainActivity.java
+++ src/fetel/hcmus/earpractice/MainActivity.java
@@ -1,15 +1,20 @@
 package fetel.hcmus.earpractice;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -25,6 +30,9 @@ public class MainActivity extends Activity {
 		rand = new Random();
 		mockhint = (TextView) findViewById(R.id.mockhint);
 		// mockhint.setText("1");
+
+		// kiểm tra có database chưa?
+
 	}
 
 	@Override
@@ -43,6 +51,11 @@ public class MainActivity extends Activity {
 		// mockhint = (TextView) findViewById(R.id.mockhint);
 		TextView result = (TextView) findViewById(R.id.result);
 		Button b = (Button) v;
+		MediaPlayer mp;
+		int[] notesMap = { R.raw._4c2, R.raw._4cs2, R.raw._4d2,
+				R.raw._4ds2, R.raw._4e2, R.raw._4f2, R.raw._4fs2, R.raw._4g2,
+				R.raw._4gs2, R.raw._4a2, R.raw._4as2, R.raw._4b2 };
+		// notesMap[1] = R.raw._4c2;
 
 		switch (v.getId()) {
 		case R.id.btn1:
@@ -57,12 +70,16 @@ public class MainActivity extends Activity {
 		case R.id.btn10:
 		case R.id.btn11:
 		case R.id.btn12:
-			if (Integer.parseInt(b.getText().toString()) == mockquestion) {
-				result.setText(getResources().getString(R.string.correct));
-				newMockQuestion();
-			} else {
-				result.setText(getResources().getString(R.string.notcorrect));
-			}
+			mp = MediaPlayer.create(this,
+					notesMap[Integer.parseInt(b.getText().toString()) - 1]);
+			mp.start();
+			Toast.makeText(
+					this,
+					"mediaPlayer.start()"
+							+ Integer.toHexString(notesMap[Integer.parseInt(b.getText().toString()) - 1]),
+					Toast.LENGTH_LONG).show();
+			mp.release();
+			checkAnswer(result, b);
 			break;
 		case R.id.start:
 			newMockQuestion();
@@ -70,9 +87,19 @@ public class MainActivity extends Activity {
 		}
 	}
 
+	private void checkAnswer(TextView result, Button b) {
+
+		if (Integer.parseInt(b.getText().toString()) == mockquestion) {
+			result.setText(getResources().getString(R.string.correct));
+			newMockQuestion();
+		} else {
+			result.setText(getResources().getString(R.string.notcorrect));
+		}
+	}
+
 	private void newMockQuestion() {
 		// generate random answer:
-		mockquestion = rand.nextInt(11);
+		mockquestion = rand.nextInt(11 + 1);
 		mockhint.setText(mockquestion.toString());
 	}
 }
